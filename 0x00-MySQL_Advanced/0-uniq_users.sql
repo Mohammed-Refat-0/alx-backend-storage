3f2cbca1e249e4d711b17ef09e0393f16ccd2fa9
Mohammed Refat <mohammedrefat53@outlook.com>
2024-08-10 04:56:33 +0300

    task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Description: Create a table to store unique users
 
-CREATE IF NOT EXISTS users(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL
 );
