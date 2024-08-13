f703f3f69e1d04014c11967c4d309341b8dbc299
Mohammed Refat <mohammedrefat53@outlook.com>
2024-08-13 20:16:15 +0300

    task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' list_all module.
+'''
+
+
+def list_all(mongo_collection):
+    '''Lists all documents in a collection.
+    '''
+    return [document for document in mongo_collection.find()]
